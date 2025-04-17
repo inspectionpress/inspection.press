fedda7d9e3a2c4e1869855a29af15e2490fde2a1
Ubuntu <ubuntu@ip-172-26-7-188.ec2.internal>
2025-04-17 13:05:37 +0000

    Initial import of full InspectionPress codebase

---
+++ resources/views/admin/clients/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+<div class="p-6 max-w-5xl mx-auto bg-white shadow">
+    <h2 class="text-2xl font-bold mb-4">Clients</h2>
+    <p>Client listing and management UI coming soon.</p>
+</div>
+@endsection
